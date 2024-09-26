13227f78d4b8a86ccf961cff9711e7348b114e2d
Millyanasg <Millyanasg@gmail.com>
2024-09-26 20:38:41 -0300

    replace HashRouter with MemoryRouter and add NotificationTest route

--- frontend-monitoramento-escolar/src/routers/base.tsx
+++ frontend-monitoramento-escolar/src/routers/base.tsx
@@ -1,18 +1,20 @@
-import { HashRouter, Route, Routes } from 'react-router-dom';
+import { MemoryRouter, Route, Routes } from 'react-router-dom';
 import { GPSTest } from '../pages/GPSTest';
 import { Login } from '../pages/Login';
 import { QRcodeTest } from '../pages/QRcodeTest';
 import { RouteError } from './route-error';
+import { NotificationTest } from '../pages/NotificationTest';
 export function AppRoutes() {
   return (
-    <HashRouter basename='/'>
+    <MemoryRouter basename='/'>
       <Routes>
         <Route path='/' element={<Login />} />
         <Route path='/login' element={<Login />} />
         <Route path='/gps' element={<GPSTest />} />
         <Route path='/qr-code' element={<QRcodeTest />} />
+        <Route path='/notification-test' element={<NotificationTest />} />
         <Route path='*' element={<RouteError />} />
       </Routes>
-    </HashRouter>
+    </MemoryRouter>
   );
 }
