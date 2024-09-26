4b33ddbbe77a6a68665497c5bde8eafb70bae627
Millyanasg <Millyanasg@gmail.com>
2024-09-26 20:38:21 -0300

    add Leaflet map integration to GPSTest for location display

--- frontend-monitoramento-escolar/src/pages/GPSTest.tsx
+++ frontend-monitoramento-escolar/src/pages/GPSTest.tsx
@@ -1,4 +1,6 @@
-import { useState, useEffect } from 'react';
+import 'leaflet/dist/leaflet.css';
+import { useEffect, useState } from 'react';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 import { Layout } from '../components/Layout/Layout';
 
 export function GPSTest() {
@@ -16,6 +18,25 @@ export function GPSTest() {
         <h1>GPSTest</h1>
         <p>Latitude: {location?.coords.latitude}</p>
         <p>Longitude: {location?.coords.longitude}</p>
+        {location && (
+          <MapContainer
+            bounds={[
+              [location.coords.latitude, location.coords.longitude],
+              [location.coords.latitude, location.coords.longitude],
+            ]}
+            style={{ height: '400px', width: '100%' }}
+          >
+            <TileLayer url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' />
+            <Marker
+              position={[location.coords.latitude, location.coords.longitude]}
+            >
+              <Popup>
+                You are here: {location.coords.latitude},{' '}
+                {location.coords.longitude}
+              </Popup>
+            </Marker>
+          </MapContainer>
+        )}
       </div>
     </Layout>
   );
