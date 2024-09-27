733eac4ae95a0328226ab8a87c2b796e47e21a68
Millyanasg <Millyanasg@gmail.com>
2024-09-27 08:52:06 -0300

    add authentication controller and service; implement login, register, and forgot password endpoints with DTOs

--- backend-monitoramento-escolar/src/auth/auth.controller.ts
+++ backend-monitoramento-escolar/src/auth/auth.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
+
+import { AuthService } from './auth.service';
+import { ForgotPasswordDto } from './dto/ForgotPasswordDto';
+import { LoginDto } from './dto/LoginDto';
+import { RegisterDto } from './dto/RegisterDto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Get('login')
+  login(@Query() query: LoginDto) {
+    return this.authService.login(query);
+  }
+
+  @Post('register')
+  register(@Body() body: RegisterDto) {
+    return this.authService.register(body);
+  }
+
+  @Post('forgot-password')
+  forgotPassword(@Body() body: ForgotPasswordDto) {
+    return this.authService.forgotPassword(body);
+  }
+}
--- backend-monitoramento-escolar/src/auth/auth.service.ts
+++ backend-monitoramento-escolar/src/auth/auth.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
 
+import { ForgotPasswordDto } from './dto/ForgotPasswordDto';
+import { LoginDto } from './dto/LoginDto';
+import { RegisterDto } from './dto/RegisterDto';
+
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  async getUserFromToken(token: string): Promise<any> {
+    throw new Error('Method not implemented.');
+  }
+
+  async forgotPassword(body: ForgotPasswordDto): Promise<any> {
+    throw new Error('Method not implemented.');
+  }
+
+  async register(body: RegisterDto): Promise<any> {
+    throw new Error('Method not implemented.');
+  }
+
+  async login(query: LoginDto): Promise<any> {
+    throw new Error('Method not implemented.');
+  }
+}
