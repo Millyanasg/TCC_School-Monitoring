5f648e0238ab278694a9e7211b56523e888ca1ab
Millyanasg <Millyanasg@gmail.com>
2024-09-27 09:49:46 -0300

    add UserService with CRUD operations; integrate PrismaService in UserModule and UserController

--- backend-monitoramento-escolar/src/user/user.service.ts
+++ backend-monitoramento-escolar/src/user/user.service.ts
@@ -1,4 +1,74 @@
-import { Injectable } from '@nestjs/common';
+import { PrismaService } from '@backend/prisma/prisma.service';
+import { Inject, Injectable } from '@nestjs/common';
+import { Prisma, User } from '@prisma/client';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @Inject(PrismaService)
+    private readonly prismaService: PrismaService,
+  ) {}
+
+  async createUser(data: Prisma.UserCreateInput): Promise<User> {
+    return this.prismaService.user.create({
+      data,
+    });
+  }
+
+  async updateUser(params: {
+    where: Prisma.UserWhereUniqueInput;
+    data: Prisma.UserUpdateInput;
+  }): Promise<User> {
+    const { where, data } = params;
+    return this.prismaService.user.update({
+      data,
+      where,
+    });
+  }
+
+  async deleteUser(where: Prisma.UserWhereUniqueInput): Promise<User> {
+    return this.prismaService.user.delete({
+      where,
+    });
+  }
+
+  async findUserById(where: Prisma.UserWhereUniqueInput): Promise<User | null> {
+    return this.prismaService.user.findUnique({
+      where,
+    });
+  }
+
+  async findUser(params: {
+    where: Prisma.UserWhereInput;
+    orderBy?: Prisma.UserOrderByWithRelationInput;
+  }): Promise<User | null> {
+    const { where, orderBy } = params;
+    return this.prismaService.user.findFirst({
+      where,
+      orderBy,
+    });
+  }
+
+  async findUsers(params: {
+    skip?: number;
+    take?: number;
+    cursor?: Prisma.UserWhereUniqueInput;
+    where?: Prisma.UserWhereInput;
+    orderBy?: Prisma.UserOrderByWithRelationInput;
+  }): Promise<User[]> {
+    const { skip, take, cursor, where, orderBy } = params;
+    return this.prismaService.user.findMany({
+      skip,
+      take,
+      cursor,
+      where,
+      orderBy,
+    });
+  }
+
+  async countUsers(where: Prisma.UserWhereInput): Promise<number> {
+    return this.prismaService.user.count({
+      where,
+    });
+  }
+}
