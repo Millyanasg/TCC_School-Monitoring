876a85672c20ddd28608138edc67a5fb0e207c25
Millyanasg <Millyanasg@gmail.com>
2024-09-27 09:50:07 -0300

    add JWT strategies for user roles; implement validation for refresh tokens

---
+++ backend-monitoramento-escolar/src/auth/strategies/JWT_DRIVER.strategy.ts
@@ -0,0 +1,58 @@
+import {
+  HttpException,
+  HttpStatus,
+  Inject,
+  Injectable,
+  Logger,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { AuthGuard, PassportStrategy } from '@nestjs/passport';
+import { User } from '@prisma/client';
+import type { Request } from 'express';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+class TokenPayload {
+  type: User['type'];
+}
+
+@Injectable()
+export class JwtStrategyDriver extends PassportStrategy(
+  Strategy,
+  'jwt-refresh',
+) {
+  private static logger = new Logger(JwtStrategyDriver.name);
+  constructor(@Inject(ConfigService) config: ConfigService) {
+    const JWT_SECRET = config.getOrThrow('JWT_SECRET');
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: JWT_SECRET,
+      passReqToCallback: true,
+    });
+  }
+
+  public validate(req: Request, payload: TokenPayload) {
+    const refreshTokenHeader = req.headers?.authorization as string;
+    const refreshTokenCookie = req.cookies?.refresh_token as string;
+
+    const refreshToken = refreshTokenHeader
+      ? refreshTokenHeader.split(' ')[1]
+      : refreshTokenCookie;
+
+    if (!refreshToken) {
+      throw new HttpException('No refresh token', HttpStatus.UNAUTHORIZED);
+    }
+
+    if (payload.type !== 'driver') {
+      throw new HttpException('Unauthorized', HttpStatus.UNAUTHORIZED);
+    }
+
+    return {
+      ...payload,
+      refreshToken,
+    };
+  }
+}
+
+export function useDriverUser() {
+  return AuthGuard('jwt-refresh');
+}
---
+++ backend-monitoramento-escolar/src/auth/strategies/JWT_PARENT.strategy.ts
@@ -0,0 +1,58 @@
+import {
+  HttpException,
+  HttpStatus,
+  Inject,
+  Injectable,
+  Logger,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { AuthGuard, PassportStrategy } from '@nestjs/passport';
+import { User } from '@prisma/client';
+import type { Request } from 'express';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+class TokenPayload {
+  type: User['type'];
+}
+
+@Injectable()
+export class JwtStrategyParent extends PassportStrategy(
+  Strategy,
+  'jwt-refresh',
+) {
+  private static logger = new Logger(JwtStrategyParent.name);
+  constructor(@Inject(ConfigService) config: ConfigService) {
+    const JWT_SECRET = config.getOrThrow('JWT_SECRET');
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: JWT_SECRET,
+      passReqToCallback: true,
+    });
+  }
+
+  public validate(req: Request, payload: TokenPayload) {
+    const refreshTokenHeader = req.headers?.authorization as string;
+    const refreshTokenCookie = req.cookies?.refresh_token as string;
+
+    const refreshToken = refreshTokenHeader
+      ? refreshTokenHeader.split(' ')[1]
+      : refreshTokenCookie;
+
+    if (!refreshToken) {
+      throw new HttpException('No refresh token', HttpStatus.UNAUTHORIZED);
+    }
+
+    if (payload.type !== 'parent') {
+      throw new HttpException('Unauthorized', HttpStatus.UNAUTHORIZED);
+    }
+
+    return {
+      ...payload,
+      refreshToken,
+    };
+  }
+}
+
+export function useParentUser() {
+  return AuthGuard('jwt-refresh');
+}
