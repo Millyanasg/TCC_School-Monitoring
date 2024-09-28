5c572894f14c0adc76ecfeb462f97743e49ff729
Millyanasg <Millyanasg@gmail.com>
2024-09-28 20:07:30 -0300

    feat: enhance ChildrenForm with navigation buttons and conditional removal of children

--- frontend-monitoramento-escolar/src/components/user/ChildrenForm.tsx
+++ frontend-monitoramento-escolar/src/components/user/ChildrenForm.tsx
@@ -1,16 +1,19 @@
-import { DeleteOutlined } from '@ant-design/icons';
-import { Button, Card, Form, Input } from 'antd-mobile';
-import { useShallow } from 'zustand/shallow';
-import { useRegisterStep } from '../../stores/user/useRegisterStep';
-import { Child, useParentForm } from '../../stores/user/useParentForm';
+import { DeleteOutlined, PlusOutlined } from '@ant-design/icons';
 import { useNotification } from '@frontend/stores/common/useNotification';
+import { Button, Card, Form, Input } from 'antd-mobile';
+import { LeftOutline, RightOutline } from 'antd-mobile-icons';
+import { useShallow } from 'zustand/shallow';
+import { Child, useParentForm } from '../../stores/user/useParentForm';
+import { useRegisterStep } from '../../stores/user/useRegisterStep';
 
-function AddedChildAddressCard({
+export function AddedChildAddressCard({
   child,
   index,
+  allowRemove = true,
 }: {
   child: Child;
   index: number;
+  allowRemove?: boolean;
 }) {
   const [removeChild] = useParentForm(
     useShallow((state) => [state.removeChild]),
@@ -20,9 +23,17 @@ function AddedChildAddressCard({
       style={{ marginBottom: '16px' }}
       title={`${child.name} ${child.lastName}`}
       extra={
-        <Button color='danger' size='small' onClick={() => removeChild(index)}>
-          <DeleteOutlined /> Remover
-        </Button>
+        <>
+          {allowRemove && (
+            <Button
+              color='danger'
+              size='small'
+              onClick={() => removeChild(index)}
+            >
+              <DeleteOutlined /> Remover
+            </Button>
+          )}
+        </>
       }
     >
       <div>
@@ -37,7 +48,9 @@ export function ChildrenForm() {
   const [childrenList, addChildren] = useParentForm(
     useShallow((state) => [state.children, state.addChildren]),
   );
-  const nextStep = useRegisterStep(useShallow((state) => state.nextStep));
+  const [nextStep, prevStep] = useRegisterStep(
+    useShallow((state) => [state.nextStep, state.prevStep]),
+  );
   const [form] = Form.useForm<Child>();
 
   return (
@@ -63,25 +76,38 @@ export function ChildrenForm() {
         footer={
           <>
             <Button block type='submit' color='success' size='middle'>
-              Adicionar
+              <PlusOutlined /> Adicionar
             </Button>
-            <Button
-              block
-              color='primary'
-              style={{ marginTop: '2rem' }}
-              size='middle'
-              onClick={() => {
-                if (childrenList.length === 0) {
-                  triggerNotification({
-                    content: 'Adicione pelo menos uma criança',
-                  });
-                } else {
-                  nextStep();
-                }
-              }}
-            >
-              Próximo
-            </Button>
+            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+              <Button
+                color='primary'
+                style={{ marginTop: '2rem' }}
+                size='middle'
+                onClick={() => {
+                  prevStep();
+                }}
+              >
+                <LeftOutline />
+                Anterior
+              </Button>
+              <Button
+                color='primary'
+                style={{ marginTop: '2rem' }}
+                size='middle'
+                onClick={() => {
+                  if (childrenList.length === 0) {
+                    triggerNotification({
+                      content: 'Adicione pelo menos uma criança',
+                    });
+                  } else {
+                    nextStep();
+                  }
+                }}
+              >
+                <RightOutline />
+                Próximo
+              </Button>
+            </div>
           </>
         }
       >
