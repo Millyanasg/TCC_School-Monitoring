47215c610550d62c50c604dc4d1b1f10a5429d77
Millyanasg <Millyanasg@gmail.com>
2024-09-27 22:57:08 -0300

    feat: add registration route and component for user registration

--- frontend-monitoramento-escolar/src/routers/base.tsx
+++ frontend-monitoramento-escolar/src/routers/base.tsx
@@ -4,12 +4,13 @@ import { Login } from '../pages/Login';
 import { QRcodeTest } from '../pages/QRcodeTest';
 import { RouteError } from './route-error';
 import { NotificationTest } from '../pages/NotificationTest';
+import { Register } from '../pages/Register';
 export function AppRoutes() {
   return (
     <MemoryRouter basename='/'>
       <Routes>
         <Route path='/' element={<Login />} />
-        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
         <Route path='/gps' element={<GPSTest />} />
         <Route path='/qr-code' element={<QRcodeTest />} />
         <Route path='/notification-test' element={<NotificationTest />} />
