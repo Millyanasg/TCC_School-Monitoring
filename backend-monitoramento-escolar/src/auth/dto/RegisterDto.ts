b88a5fcd4cbb4ad48172a3f13c7dd59c920c4201
Millyanasg <Millyanasg@gmail.com>
2024-09-28 09:09:19 -0300

    feat: enhance DTOs with validation for email and password fields

--- backend-monitoramento-escolar/src/auth/dto/RegisterDto.ts
+++ backend-monitoramento-escolar/src/auth/dto/RegisterDto.ts
@@ -1,6 +1,19 @@
+import { IsEmail, IsNotEmpty, Length, Matches } from 'class-validator';
+
 export class RegisterDto {
+  @IsNotEmpty()
   name: string;
+
+  @IsNotEmpty()
   lastName: string;
+
+  @IsEmail({})
   email: string;
+
+  @IsNotEmpty()
+  @Length(8, 20)
+  @Matches(/[a-z]/)
+  @Matches(/[A-Z]/)
+  @Matches(/\d/)
   password: string;
 }
