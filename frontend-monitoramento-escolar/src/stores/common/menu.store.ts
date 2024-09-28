f982e1b1580d649b36e0e833ecbeaabe6ebca85d
Millyanasg <Millyanasg@gmail.com>
2024-09-28 19:39:08 -0300

    refactor: clean up menu store by removing unused comments and simplifying type definitions

--- frontend-monitoramento-escolar/src/stores/common/menu.store.ts
+++ frontend-monitoramento-escolar/src/stores/common/menu.store.ts
@@ -1,22 +1,11 @@
 import { create } from 'zustand';
 
-/**
- * Tipo que representa o estado do menu.
- */
 type MenuState = {
-  /** Indica se o menu está aberto. */
   isOpen: boolean;
-  /** Função para alternar o estado do menu. */
   toggleMenu: () => void;
-  /** Função para fechar o menu. */
   closeMenu: () => void;
 };
 
-/**
- * Hook para gerenciar o estado do menu.
- *
- * @returns {MenuState} O estado e funções para controlar o menu.
- */
 export const useMenuStore = create<MenuState>((set) => ({
   isOpen: false,
   toggleMenu: () => set((state) => ({ isOpen: !state.isOpen })),
