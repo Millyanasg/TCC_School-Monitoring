34dc973535f8601a4c1d81736c8748149f7e44af
Millyanasg <Millyanasg@gmail.com>
2024-09-27 22:20:58 -0300

    refactor: replace Logger with logger for consistent logging in development and production

--- backend-monitoramento-escolar/src/main.ts
+++ backend-monitoramento-escolar/src/main.ts
@@ -34,10 +34,10 @@ async function bootstrap() {
 
   if (process.env.NODE_ENV === 'development') {
     initializeSwagger(app, port, serverUrl);
-    Logger.debug('Swagger initialized');
+    logger.debug('Swagger initialized');
   } else {
     // disable debug logs in production
-    Logger.debug('Swagger not initialized');
+    logger.debug('Swagger not initialized');
 
     Logger.prototype.debug = () => undefined;
   }
@@ -51,7 +51,7 @@ async function bootstrap() {
         //Logger.debug(`Allowed origin: ${origin}`);
         callback(null, true);
       } else {
-        Logger.debug(`Blocked origin: ${origin}`);
+        logger.debug(`Blocked origin: ${origin}`);
         callback(new Error('Not allowed by CORS'));
       }
     },
