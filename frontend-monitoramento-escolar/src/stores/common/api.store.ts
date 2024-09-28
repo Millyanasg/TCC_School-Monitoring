635ab41ea515a9465b5412c2d0df380e8f1d96c0
Millyanasg <Millyanasg@gmail.com>
2024-09-27 22:56:35 -0300

    refactor: update API base URL configuration to use VITE_API_BASE environment variable

--- frontend-monitoramento-escolar/src/stores/common/api.store.ts
+++ frontend-monitoramento-escolar/src/stores/common/api.store.ts
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-const baseURL =
-  process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000/api/v1';
+const baseURL = import.meta.env.VITE_API_BASE as string;
 
 /**
  * An Axios instance configured with default settings for making API requests.
