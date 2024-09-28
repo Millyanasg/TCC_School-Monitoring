08dc00f38d4214e0604cef3ac2c83795ad3fe3b3
Millyanasg <Millyanasg@gmail.com>
2024-09-28 19:38:09 -0300

    feat: implement user registration form with validation and notification handling

--- frontend-monitoramento-escolar/src/pages/Register.tsx
+++ frontend-monitoramento-escolar/src/pages/Register.tsx
@@ -1,36 +1,60 @@
-import { Button, Form, FormProps, Input, Toast } from 'antd-mobile';
-import { Layout } from '../components/Layout/Layout';
+import { Layout } from '@frontend/components/Layout/Layout';
+import { registerUser } from '@frontend/services/common/auth.service';
+import { useNotification } from '@frontend/stores/common/useNotification';
+import { Button, Form, Input } from 'antd-mobile';
 import { useNavigate } from 'react-router-dom';
+import { RegisterDto } from '@backend/auth/dto/RegisterDto';
 
 export function Register() {
+  const { triggerNotification } = useNotification();
   const navigate = useNavigate();
-  const onFinish = (values: { username: string; password: string }) => {
-    // Handle login logic here
-    console.log('Success:', values);
-    Toast.show({
-      icon: 'success',
-      content: 'Login successful',
-    });
-  };
+  const [form] = Form.useForm<RegisterDto>();
 
-  const onFinishFailed: FormProps['onFinishFailed'] = (errorInfo) => {
-    console.log('Failed:', errorInfo);
-    Toast.show({
-      icon: 'fail',
-      content: 'Login failed',
-    });
+  const onSubmit = () => {
+    const { name, lastName, email, password } = form.getFieldsValue();
+    // Handle login logic here
+    registerUser({ name, lastName, email, password })
+      .then(() => {
+        triggerNotification({
+          content: 'Conta criada com sucesso',
+        });
+        navigate('/menu');
+      })
+      .catch((erro) => {
+        if (erro.isAxiosError) {
+          if (erro.response?.status === 409) {
+            triggerNotification({
+              content: 'Usuário já existe',
+            });
+            // set erro the the email field
+            form.setFields([
+              {
+                name: 'email',
+                errors: ['Usuário já existe com este email'],
+              },
+            ]);
+          } else {
+            triggerNotification({
+              content: 'Erro ao criar conta',
+            });
+          }
+        } else {
+          triggerNotification({
+            content: 'Erro ao criar conta',
+          });
+        }
+      });
   };
 
   return (
     <Layout>
       <div style={{ padding: '16px' }}>
         <Form
-          layout='horizontal'
-          onFinish={onFinish}
-          onFinishFailed={onFinishFailed}
+          form={form}
+          layout='vertical'
           footer={
             <>
-              <Button block type='submit' color='success' size='middle'>
+              <Button block onClick={onSubmit} color='success' size='middle'>
                 Criar conta
               </Button>
               <Button
@@ -48,36 +72,80 @@ export function Register() {
           <Form.Item
             name='name'
             label='Primeiro nome'
-            rules={[{ required: true, message: 'Por favor, digite seu nome!' }]}
+            rules={[{ required: true, message: 'Por favor, digite seu nome' }]}
           >
-            <Input placeholder='Username' />
+            <Input placeholder='Username' autoComplete='given-name' />
           </Form.Item>
           <Form.Item
             name='lastName'
             label='Sobrenome'
             rules={[
-              { required: true, message: 'Por favor, digite seu sobrenome!' },
+              { required: true, message: 'Por favor, digite seu sobrenome' },
             ]}
           >
-            <Input placeholder='Username' />
+            <Input placeholder='Username' autoComplete='family-name' />
           </Form.Item>
           <Form.Item
             name='email'
             label='Email de usuário'
-            rules={[
-              { required: true, message: 'Por favor, insira seu email!' },
-            ]}
+            rules={[{ required: true, message: 'Por favor, insira seu email' }]}
           >
-            <Input placeholder='Username' />
+            <Input placeholder='Username' autoComplete='email' />
           </Form.Item>
           <Form.Item
             name='password'
             label='Senha'
             rules={[
-              { required: true, message: 'Por favor, insira sua senha!' },
+              { required: true, message: 'Por favor, insira sua senha' },
+              {
+                pattern: /[a-z]/,
+                message: 'A senha deve conter pelo menos uma letra minúscula',
+              },
+              {
+                pattern: /[A-Z]/,
+                message: 'A senha deve conter pelo menos uma letra maiúscula',
+              },
+              {
+                pattern: /\d/,
+                message: 'A senha deve conter pelo menos um número',
+              },
+              {
+                min: 8,
+                message: 'A senha deve ter pelo menos 8 caracteres',
+              },
             ]}
           >
-            <Input type='password' placeholder='********' />
+            <Input
+              type='password'
+              placeholder='********'
+              autoComplete='new-password'
+            />
+          </Form.Item>
+          <Form.Item
+            name='confirmPassword'
+            label='Confirme sua senha'
+            rules={[
+              { required: true, message: 'Por favor, confirme sua senha' },
+              {
+                validator(_, value) {
+                  if (value !== undefined) {
+                    const password = form.getFieldValue('password') as string;
+                    if (value !== password) {
+                      return Promise.reject(
+                        new Error('As senhas não são iguais'),
+                      );
+                    }
+                  }
+                  return Promise.resolve();
+                },
+              },
+            ]}
+          >
+            <Input
+              type='password'
+              placeholder='********'
+              autoComplete='new-password'
+            />
           </Form.Item>
         </Form>
       </div>
