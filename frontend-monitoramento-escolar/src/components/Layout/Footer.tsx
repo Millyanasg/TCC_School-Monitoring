069fabdeabf4dcbb3d873cda1993e21662b4280e
Millyanasg <Millyanasg@gmail.com>
2024-09-27 22:49:20 -0300

    feat: enhance Footer component with icons and fixed positioning

--- frontend-monitoramento-escolar/src/components/Layout/Footer.tsx
+++ frontend-monitoramento-escolar/src/components/Layout/Footer.tsx
@@ -1,10 +1,28 @@
 import { TabBar } from 'antd-mobile';
+import {
+  CheckShieldOutline,
+  EnvironmentOutline,
+  SetOutline,
+} from 'antd-mobile-icons';
 
 export function Footer() {
   return (
-    <TabBar>
-      <TabBar.Item title='Home' key='home' />
-      <TabBar.Item title='Settings' key='settings' />
-    </TabBar>
+    <div
+      style={{
+        position: 'fixed',
+        bottom: 0,
+        width: '100%',
+        background: '#D0D0D0',
+      }}
+    >
+      <TabBar>
+        <TabBar.Item title={<CheckShieldOutline fontSize={38} />} key='gps' />
+        <TabBar.Item
+          title={<EnvironmentOutline fontSize={38} />}
+          key='confirm'
+        />
+        <TabBar.Item title={<SetOutline fontSize={38} />} key='settings' />
+      </TabBar>
+    </div>
   );
 }
