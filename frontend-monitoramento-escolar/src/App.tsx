da2142d4733aafc905da758c995a1aea03b2204d
Millyanasg <Millyanasg@gmail.com>
2024-09-27 22:56:52 -0300

    feat: wrap AppRoutes in a main element for improved layout and styling

--- frontend-monitoramento-escolar/src/App.tsx
+++ frontend-monitoramento-escolar/src/App.tsx
@@ -1,5 +1,17 @@
 import { AppRoutes } from './routers/base';
 
 export function App() {
-  return <AppRoutes />;
+  return (
+    <main
+      style={{
+        maxWidth: '100vw',
+        minHeight: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+      }}
+    >
+      <AppRoutes />
+    </main>
+  );
 }
