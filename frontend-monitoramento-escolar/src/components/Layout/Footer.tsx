db5e35c5ab979ba73c805e928760298abf3b8e5d
Millyanasg <Millyanasg@gmail.com>
2024-09-28 19:38:00 -0300

    feat: enhance Footer component with user-based tab navigation and styling updates

--- frontend-monitoramento-escolar/src/components/Layout/Footer.tsx
+++ frontend-monitoramento-escolar/src/components/Layout/Footer.tsx
@@ -1,28 +1,60 @@
+import { useUserStore } from '@frontend/stores/user/user.store';
 import { TabBar } from 'antd-mobile';
 import {
   CheckShieldOutline,
   EnvironmentOutline,
   SetOutline,
 } from 'antd-mobile-icons';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 export function Footer() {
+  const userData = useUserStore((state) => state.userData);
+  const navigate = useNavigate();
+  const location = useLocation();
+  // const isUsetUser = useMemo(() => userData?.type === 'unset', [userData]);
+  const setRouteActive = (value: string) => {
+    navigate(value);
+  };
+  const tabs = [
+    {
+      key: 'menu',
+      icon: <CheckShieldOutline fontSize={38} />,
+    },
+    {
+      key: 'confirm',
+      icon: <EnvironmentOutline fontSize={38} />,
+    },
+    {
+      key: 'settings',
+      icon: <SetOutline fontSize={38} />,
+    },
+  ];
   return (
     <div
       style={{
         position: 'fixed',
         bottom: 0,
         width: '100%',
-        background: '#D0D0D0',
+        background: 'black',
+        color: 'white',
+        padding: '0.5rem 0',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
       }}
     >
-      <TabBar>
-        <TabBar.Item title={<CheckShieldOutline fontSize={38} />} key='gps' />
-        <TabBar.Item
-          title={<EnvironmentOutline fontSize={38} />}
-          key='confirm'
-        />
-        <TabBar.Item title={<SetOutline fontSize={38} />} key='settings' />
-      </TabBar>
+      {userData && (
+        <TabBar
+          activeKey={location.pathname}
+          onChange={(value) => setRouteActive(value)}
+          style={{ width: '100%' }}
+        >
+          {tabs.map((item) => (
+            <TabBar.Item key={item.key} icon={item.icon} />
+          ))}
+        </TabBar>
+      )}
     </div>
   );
 }
