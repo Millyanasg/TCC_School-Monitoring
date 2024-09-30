b6165d5da2446b2723dd8a971b83a68c0c7fe2ae
Millyanasg <Millyanasg@gmail.com>
2024-09-29 22:02:40 -0300

    feat: add Zustand store for managing user geolocation position tracking

---
+++ frontend-monitoramento-escolar/src/stores/common/position.store.ts
@@ -0,0 +1,71 @@
+import { create } from 'zustand';
+
+interface PositionState {
+  location: GeolocationPosition | null;
+  initializePosition: () => () => void;
+}
+/**
+ * Custom hook to manage the user's geolocation position using Zustand and local storage.
+ *
+ * @returns {PositionState} The state containing the user's current location and a function to initialize position tracking.
+ *
+ * @function initializePosition
+ * Initializes the geolocation tracking. It gets the current position and sets up a watcher to track position changes.
+ *
+ * @returns {void}
+ *
+ * @example
+ * ```tsx
+ * const { location, initializePosition } = usePositionStore();
+ *
+ * useEffect(() => {
+ *  initializePosition();
+ * }, []);
+ * ```
+ *
+ * @typedef {Object} PositionState
+ * @property {GeolocationPosition | null} location - The current geolocation position of the user.
+ * @property {Function} initializePosition - Function to start tracking the user's position.
+ */
+export const usePositionStore = create<PositionState>((set) => {
+  function initializePosition() {
+    // Get the current position
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        set({ location: position });
+      },
+      (error) => {
+        console.error('Error getting current position:', error);
+      },
+      {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0,
+      },
+    );
+
+    // Watch the position as it changes
+    const watchId = navigator.geolocation.watchPosition(
+      (position) => {
+        set({ location: position });
+        console.log('Position changed:', position);
+      },
+      (error) => {
+        console.error('Error watching position:', error);
+      },
+      {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0,
+      },
+    );
+
+    return () => {
+      navigator.geolocation.clearWatch(watchId);
+    };
+  }
+  return {
+    location: null,
+    initializePosition,
+  };
+});
