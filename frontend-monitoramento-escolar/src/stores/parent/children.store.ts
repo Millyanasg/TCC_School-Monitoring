e5f5e5914e4266c1bf66c7085ba5420ab5e89894
Millyanasg <Millyanasg@gmail.com>
2024-10-03 21:59:39 -0300

    feat: add Zustand store for managing children state with CRUD operations

---
+++ frontend-monitoramento-escolar/src/stores/parent/children.store.ts
@@ -0,0 +1,98 @@
+import { ChildViewDto } from '@backend/parent/dto/ChildViewDto';
+import {
+  fetchChildren,
+  updateChildren,
+  addChildren,
+  removeChildren,
+} from '@frontend/services/parent/children.service';
+import { create } from 'zustand';
+
+type ChildrenStore = {
+  children: ChildViewDto[];
+  selectedChild: ChildViewDto | null;
+  isEditing: boolean;
+  setIsEditing: (isEditing: boolean) => boolean;
+  setSelectedChild: (child: ChildViewDto | null) => ChildViewDto | null;
+  updateChildren: (children: ChildViewDto) => Promise<ChildViewDto>;
+  addChildren: (children: ChildViewDto) => Promise<ChildViewDto>;
+  removeChildren: (children: ChildViewDto) => Promise<ChildViewDto>;
+};
+
+/**
+ * A Zustand store for managing the state of children.
+ *
+ * @returns {ChildrenStore} The Zustand store with the following properties and methods:
+ *
+ * @property {ChildViewDto[]} children - The list of children.
+ *
+ * @method updateChildren - Updates the state of a specific child.
+ * @param {ChildViewDto} children - The child data to update.
+ * @returns {Promise<void>} A promise that resolves when the state is updated.
+ *
+ * @method addChildren - Adds a new child to the state.
+ * @param {ChildViewDto} child - The new child data to add.
+ * @returns {Promise<void>} A promise that resolves when the state is updated.
+ *
+ * @method removeChildren - Removes a child from the state.
+ * @param {ChildViewDto} child - The child data to remove.
+ * @returns {Promise<void>} A promise that resolves when the state is updated.
+ *
+ * @method updateState - Fetches the list of children and updates the state.
+ * @returns {Promise<void>} A promise that resolves when the state is updated.
+ *
+ * @example
+ * ```tsx
+ * const { children, updateChildren, addChildren, removeChildren, setSelectedChild, selectedChild } = useChildrenStore();
+ *
+ * useEffect(() => {
+ *  console.log(children);
+ * }, []);
+ * ```
+ */
+export const useChildrenStore = create<ChildrenStore>((set) => {
+  async function updateChildrenState(children: ChildViewDto) {
+    const updatedChildren = await updateChildren(children);
+    set((state) => ({
+      children: state.children.map((c) =>
+        c.id === updatedChildren.id ? updatedChildren : c,
+      ),
+    }));
+    return updatedChildren;
+  }
+  async function addChildrenState(child: ChildViewDto) {
+    const newChild = await addChildren(child);
+    set((state) => ({ children: [...state.children, newChild] }));
+    return newChild;
+  }
+  async function removeChildrenState(child: ChildViewDto) {
+    await removeChildren(child);
+    set((state) => ({
+      children: state.children.filter((c) => c.id !== child.id),
+    }));
+    return child;
+  }
+
+  async function updateState() {
+    const childList = await fetchChildren();
+    set({ children: childList });
+  }
+
+  updateState();
+
+  return {
+    children: [],
+    selectedChild: null,
+    setSelectedChild: (child) => {
+      set({ selectedChild: child });
+      return child;
+    },
+    isEditing: false,
+    setIsEditing: (isEditing) => {
+      set({ isEditing });
+      return isEditing;
+    },
+    updateChildren: updateChildrenState,
+    addChildren: addChildrenState,
+    removeChildren: removeChildrenState,
+  };
+});
