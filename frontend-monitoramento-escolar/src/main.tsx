1726739652b847be076e77802bfd5a4ceadd56bc
Millyanasg <Millyanasg@gmail.com>
2024-10-03 21:59:04 -0300

    feat: wrap App component with ConfigProvider to set locale to Portuguese (pt-BR)

--- frontend-monitoramento-escolar/src/main.tsx
+++ frontend-monitoramento-escolar/src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
+import { ConfigProvider } from 'antd';
+import ptBR from 'antd/lib/locale/pt_BR';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ConfigProvider locale={ptBR}>
+      <App />
+    </ConfigProvider>
   </StrictMode>,
 );
