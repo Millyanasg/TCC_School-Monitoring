1edcd400156ae1850058abe20aad08f4587b052b
Millyanasg <Millyanasg@gmail.com>
2024-10-07 11:44:49 -0300

    feat: add Zustand store for managing driver car state and updates

---
+++ frontend-monitoramento-escolar/src/stores/driver/driverCar.store.ts
@@ -0,0 +1,50 @@
+import { create } from 'zustand';
+import { DriverDto } from '@backend/driver/dto/DriverDto';
+import { DriverViewDto } from '@backend/driver/dto/DriverViewDto';
+import {
+  getDriver,
+  updateDriver,
+} from '@frontend/services/driver/driverCar.service';
+type DriverCar = {
+  driver: DriverViewDto | null;
+  updateDriver: (data: DriverDto) => Promise<void>;
+  isEditing: boolean;
+  setIsEditing: (isEditing: boolean) => void;
+};
+
+/**
+ * Custom hook to manage the state of a driver car.
+ *
+ * This hook uses the `create` function to initialize the state with a driver object.
+ * It fetches the driver data asynchronously and updates the state accordingly.
+ *
+ * @returns {object} An object containing the driver state and a function to update the driver state.
+ *
+ * @property {Driver | null} driver - The current driver state, initially set to null.
+ * @property {function} updateDriver - An asynchronous function to update the driver state.
+ *
+ * @example
+ * const { driver, updateDriver, isEditing, setIsEditing } = useDriverCar();
+ *
+ * // To update the driver state
+ * updateDriver(newDriverData);
+ */
+export const useDriverCar = create<DriverCar>((set) => {
+  getDriver().then((respose) => {
+    const driver = respose.data;
+    set({ driver });
+  });
+
+  async function updateDriverState(data: DriverDto) {
+    const respose = await updateDriver(data);
+    const driver = respose.data;
+    set({ driver });
+  }
+
+  return {
+    driver: null,
+    updateDriver: updateDriverState,
+    isEditing: false,
+    setIsEditing: (isEditing: boolean) => set({ isEditing }),
+  };
+});
