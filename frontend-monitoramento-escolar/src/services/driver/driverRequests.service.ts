97180c8def39e3b97a6fb30fc52e7e6d910212d4
Millyanasg <Millyanasg@gmail.com>
2024-10-14 13:25:56 -0300

    feat: update driver requests service to include fetching, accepting, and declining requests

--- frontend-monitoramento-escolar/src/services/driver/driverRequests.service.ts
+++ frontend-monitoramento-escolar/src/services/driver/driverRequests.service.ts
@@ -1,5 +1,11 @@
 import { apiInstance } from '@frontend/stores/common/api.store';
-
-export async function getDriverTrips() {
-  return await apiInstance.get('/driver/request');
+import { DriverRequestInfoViewDto } from '@backend/driver/dto/DriverRequestInfoViewDto';
+export async function getDriverRequests() {
+  return apiInstance.get<DriverRequestInfoViewDto[]>('/driver/requests');
+}
+export async function acceptDriverRequest(id: number) {
+  return apiInstance.put('/driver/requests', null, { params: { id } });
+}
+export async function declineDriverRequest(id: number) {
+  return apiInstance.delete('/driver/requests', { params: { id } });
 }
