150b2a6823af9a9689c9faf45d40d9238275a96a
Millyanasg <Millyanasg@gmail.com>
2024-10-15 08:34:31 -0300

    feat: refactor SeedService to use Prisma and Crypt services for user seeding

--- backend-monitoramento-escolar/src/seed/seed.service.ts
+++ backend-monitoramento-escolar/src/seed/seed.service.ts
@@ -1,104 +1,80 @@
-import { UserService } from '@backend/user/user.service';
+import { CryptService } from '@backend/crypt/crypt.service';
+import { PrismaService } from '@backend/prisma/prisma.service';
+import {
+  HttpException,
+  HttpStatus,
+  Inject,
+  Injectable,
+  Logger,
+} from '@nestjs/common';
+import { Seed } from '@prisma/client';
 import { faker } from '@faker-js/faker';
-import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class SeedService {
-  constructor(private readonly userService: UserService) {}
-  public async seed() {
-    throw new Error('Method not implemented.');
-  }
-
-  private getRandomAddress() {
-    const country = 'Brazil';
-    const state = 'RJ';
-    const city = 'Rio de Janeiro';
-    const street = `Rua ${faker.location.street()}`;
-    const number = Math.floor(Math.random() * 1000);
-    const zipCode = faker.location.zipCode();
-
-    const latitude = faker.location.latitude({
-      max: -22.0,
-      min: -23.0,
-      precision: 0.0001,
+  private readonly logger = new Logger(SeedService.name);
+  constructor(
+    @Inject(PrismaService)
+    private readonly prismaService: PrismaService,
+    @Inject(CryptService)
+    private readonly cryptService: CryptService,
+  ) {}
+  private async verifyIfSeeded(type: Seed['type']) {
+    const seeded = await this.prismaService.seed.findUnique({
+      where: {
+        type: type,
+      },
     });
 
-    const longitude = faker.location.longitude({
-      max: -43.0,
-      min: -44.0,
-      precision: 0.0001,
-    });
+    this.logger.log(`Seeded ${type}: ${seeded ? 'found' : 'not found'}`);
 
-    return {
-      country,
-      state,
-      city,
-      street,
-      number,
-      zipCode,
-      latitude,
-      longitude,
-    };
-  }
-
-  async seedParentUsers() {
-    for (let i = 0; i < 100; i++) {
-      const user = this.createFakeUser();
-      // const address = await this.getRandomAddress();
-      // const children = this.createFakeChildren();
-
-      await this.userService.createUser({
-        name: user.firstName,
-        lastName: user.lastName,
-        email: user.email,
-        password: faker.internet.password(),
-        type: 'parent',
-      });
+    if (seeded) {
+      throw new HttpException('Already seeded', HttpStatus.BAD_REQUEST);
     }
   }
 
-  private createFakeChildren() {
-    return Array.from({ length: Math.floor(Math.random() * 5) }, () =>
-      this.generateFakeStudent(),
-    );
+  private async markAsSeeded({ type }: { type: Seed['type'] }) {
+    return await this.prismaService.seed.create({
+      data: {
+        type,
+      },
+    });
   }
 
-  private generateFakeStudent() {
-    const grades = [
-      'C.A',
-      '2º ano',
-      '3º ano',
-      '4º ano',
-      '5º ano',
-      '6º ano',
-      '7º ano',
-      '8º ano',
-      '9º ano',
-      '1º ano',
-      '2º ano',
-      '3º ano',
-    ];
+  async seedUsers() {
+    const total = 4 * 20;
+    for (let i = 0; i < total; i++) {
+      const name = faker.person.firstName();
+      const lastName = faker.person.lastName();
+      const email = faker.internet.email();
+      await this.prismaService.user.create({
+        data: {
+          name,
+          lastName,
+          email,
+          password: await this.cryptService.hashPassword('12345678'),
+          type: 'unset',
+        },
+      });
+      this.logger.debug(`User ${name} ${lastName} created`);
+    }
 
-    const name = faker.person.firstName();
-    const lastName = faker.person.lastName();
-    const age = Math.floor(Math.random() * 10) + 6; // 6 to 13
-    const grade = grades[age - 6];
-    const schoolId = Math.floor(Math.random() * 1000);
-    const driverId = Math.floor(Math.random() * 1000);
-    return { name, lastName, age, grade, schoolId, driverId };
+    // the first 20 are drivers
+
+    // the second 20 are parents
   }
 
-  private createFakeUser() {
-    const firstName = faker.person.firstName();
-    const lastName = faker.person.lastName();
+  async seed() {
+    try {
+      await this.verifyIfSeeded('COMMON');
 
-    const email = faker.internet
-      .email({
-        firstName,
-        lastName,
-      })
-      .toLowerCase();
-
-    return { firstName, lastName, email };
+      return 'Seeded';
+    } catch (error) {
+      this.logger.error(error);
+      throw new HttpException(
+        'Error seeding',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 }
