fa6034794e81717bd505a510c1f260aa20a09aa2
Millyanasg <Millyanasg@gmail.com>
2024-10-15 09:47:09 -0300

    refactor: remove DriverRequestController and related tests from Driver module

--- backend-monitoramento-escolar/src/driver/driver.module.ts
+++ backend-monitoramento-escolar/src/driver/driver.module.ts
@@ -3,11 +3,10 @@ import { Module } from '@nestjs/common';
 
 import { DriverController } from './driver.controller';
 import { DriverService } from './driver.service';
-import { DriverRequestController } from './driverRequestController.controller';
 
 @Module({
   imports: [PrismaModule],
   providers: [DriverService],
-  controllers: [DriverController, DriverRequestController],
+  controllers: [DriverController],
 })
 export class DriverModule {}
