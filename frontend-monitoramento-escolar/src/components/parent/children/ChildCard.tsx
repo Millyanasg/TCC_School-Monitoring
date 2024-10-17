9aa8a49dbecd6270487e0c861840facc59eb0401
Millyanasg <Millyanasg@gmail.com>
2024-10-17 16:27:56 -0300

    feat: add QR code modal to ChildCard component for displaying and printing QR codes

--- frontend-monitoramento-escolar/src/components/parent/children/ChildCard.tsx
+++ frontend-monitoramento-escolar/src/components/parent/children/ChildCard.tsx
@@ -1,10 +1,88 @@
-import { DeleteOutlined, EditOutlined } from '@ant-design/icons';
+import {
+  CloseCircleFilled,
+  DeleteOutlined,
+  EditOutlined,
+  PrinterOutlined,
+  QrcodeOutlined,
+} from '@ant-design/icons';
 import { ChildViewDto } from '@backend/parent/dto/ChildViewDto';
+import { QRCodeDrawer } from '@frontend/components/common/QRcode/QRCodeDrawer';
 import { useChildrenStore } from '@frontend/stores/parent/children.store';
-import { Button } from 'antd';
+import { Button, Flex, Modal, Typography } from 'antd';
 import { Card } from 'antd-mobile';
 import dayjs from 'dayjs';
+import { useState } from 'react';
 import { useShallow } from 'zustand/shallow';
+
+const QrCodeModal = ({
+  isOpen,
+  setIsOpen,
+  child,
+}: {
+  isOpen: boolean;
+  setIsOpen: (isOpen: boolean) => void;
+  child: ChildViewDto;
+}) => {
+  const onClose = () => {
+    setIsOpen(false);
+  };
+
+  const onPrint = () => {
+    window.print();
+  };
+  return (
+    <Modal
+      open={isOpen}
+      onClose={onClose}
+      footer={null}
+      height={'80vh'}
+      width={'80vw'}
+    >
+      <div>
+        <Typography.Title level={4}>
+          QRCode de {child.name} {child.lastName} <QrcodeOutlined />
+        </Typography.Title>
+        <Flex gap='2px' vertical align='start'>
+          <Typography.Text>
+            Data de nascimento: {dayjs(child.birthDate).format('DD/MM/YYYY')}
+          </Typography.Text>
+          <Typography.Text>Ano escolar: {child.grade}</Typography.Text>
+        </Flex>
+        <Flex vertical align='center'>
+          <Typography.Text strong>
+            Escaneie o QRCode abaixo para liberar ou receber {child.name}{' '}
+            {child.lastName}
+          </Typography.Text>
+          <QRCodeDrawer
+            style={{
+              width: '400px',
+              height: '400px',
+            }}
+            qrCodePayload={String(child.id)}
+          />
+        </Flex>
+        <Flex gap='1rem'>
+          <Button
+            type='primary'
+            color='primary'
+            icon={<PrinterOutlined />}
+            onClick={onPrint}
+          >
+            Impimir ou compartilhar código
+          </Button>
+          <Button
+            type='primary'
+            color='danger'
+            icon={<CloseCircleFilled />}
+            onClick={onClose}
+          >
+            Fechar
+          </Button>
+        </Flex>
+      </div>
+    </Modal>
+  );
+};
 
 export function ChildCard({ child }: { child: ChildViewDto }) {
   const [deleteSelectedChild, setSelectedChild, setOpenModal] =
@@ -15,46 +93,66 @@ export function ChildCard({ child }: { child: ChildViewDto }) {
         state.setIsEditing,
       ]),
     );
-  function removeChild() {
+
+  const [isQrCodeModalOpen, setIsQrCodeModalOpen] = useState(false);
+
+  const removeChild = () => {
     setSelectedChild(child);
     deleteSelectedChild(child);
-  }
-  function editChild() {
+  };
+  const editChild = () => {
     setOpenModal(true);
     setSelectedChild(child);
-  }
+  };
+
+  const genQRCode = () => {
+    setIsQrCodeModalOpen(true);
+  };
 
   return (
     <Card
       style={{ marginBottom: '16px' }}
       title={`${child.name} ${child.lastName}`}
-      extra={
-        <>
-          <Button
-            onClick={removeChild}
-            color='danger'
-            variant='solid'
-            size='small'
-            icon={<DeleteOutlined />}
-          >
-            Excluir
-          </Button>
-          <Button
-            onClick={editChild}
-            color='primary'
-            variant='solid'
-            size='small'
-            icon={<EditOutlined />}
-          >
-            Editar
-          </Button>
-        </>
-      }
+      extra={null}
     >
-      <div>
+      <QrCodeModal
+        isOpen={isQrCodeModalOpen}
+        setIsOpen={setIsQrCodeModalOpen}
+        child={child}
+      />
+      <Flex gap='1rem'>
         <p>Data de nascimento: {dayjs(child.birthDate).format('DD/MM/YYYY')}</p>
         <p>Ano escolar: {child.grade}</p>
-      </div>
+      </Flex>
+      <Flex gap='1rem'>
+        <Button
+          onClick={removeChild}
+          color='danger'
+          variant='solid'
+          size='small'
+          icon={<DeleteOutlined />}
+        >
+          Excluir
+        </Button>
+        <Button
+          onClick={editChild}
+          color='primary'
+          variant='solid'
+          size='small'
+          icon={<EditOutlined />}
+        >
+          Editar
+        </Button>
+        <Button
+          onClick={genQRCode}
+          color='primary'
+          variant='solid'
+          size='small'
+          icon={<QrcodeOutlined />}
+        >
+          Ver QRCode
+        </Button>
+      </Flex>
     </Card>
   );
 }
