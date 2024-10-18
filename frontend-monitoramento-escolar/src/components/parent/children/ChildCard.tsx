41191ca1c6aa578cb37e6c4f0efe19d3a4c021ee
Millyanasg <Millyanasg@gmail.com>
2024-10-18 09:31:36 -0300

    feat: extract QrCodeModal component for improved QR code display and printing functionality

--- frontend-monitoramento-escolar/src/components/parent/children/ChildCard.tsx
+++ frontend-monitoramento-escolar/src/components/parent/children/ChildCard.tsx
@@ -1,88 +1,16 @@
 import {
-  CloseCircleFilled,
   DeleteOutlined,
   EditOutlined,
-  PrinterOutlined,
   QrcodeOutlined,
 } from '@ant-design/icons';
 import { ChildViewDto } from '@backend/parent/dto/ChildViewDto';
-import { QRCodeDrawer } from '@frontend/components/common/QRcode/QRCodeDrawer';
 import { useChildrenStore } from '@frontend/stores/parent/children.store';
-import { Button, Flex, Modal, Typography } from 'antd';
+import { Button, Flex } from 'antd';
 import { Card } from 'antd-mobile';
 import dayjs from 'dayjs';
 import { useState } from 'react';
 import { useShallow } from 'zustand/shallow';
-
-const QrCodeModal = ({
-  isOpen,
-  setIsOpen,
-  child,
-}: {
-  isOpen: boolean;
-  setIsOpen: (isOpen: boolean) => void;
-  child: ChildViewDto;
-}) => {
-  const onClose = () => {
-    setIsOpen(false);
-  };
-
-  const onPrint = () => {
-    window.print();
-  };
-  return (
-    <Modal
-      open={isOpen}
-      onClose={onClose}
-      footer={null}
-      height={'80vh'}
-      width={'80vw'}
-    >
-      <div>
-        <Typography.Title level={4}>
-          QRCode de {child.name} {child.lastName} <QrcodeOutlined />
-        </Typography.Title>
-        <Flex gap='2px' vertical align='start'>
-          <Typography.Text>
-            Data de nascimento: {dayjs(child.birthDate).format('DD/MM/YYYY')}
-          </Typography.Text>
-          <Typography.Text>Ano escolar: {child.grade}</Typography.Text>
-        </Flex>
-        <Flex vertical align='center'>
-          <Typography.Text strong>
-            Escaneie o QRCode abaixo para liberar ou receber {child.name}{' '}
-            {child.lastName}
-          </Typography.Text>
-          <QRCodeDrawer
-            style={{
-              width: '400px',
-              height: '400px',
-            }}
-            qrCodePayload={String(child.id)}
-          />
-        </Flex>
-        <Flex gap='1rem'>
-          <Button
-            type='primary'
-            color='primary'
-            icon={<PrinterOutlined />}
-            onClick={onPrint}
-          >
-            Impimir ou compartilhar código
-          </Button>
-          <Button
-            type='primary'
-            color='danger'
-            icon={<CloseCircleFilled />}
-            onClick={onClose}
-          >
-            Fechar
-          </Button>
-        </Flex>
-      </div>
-    </Modal>
-  );
-};
+import { QrCodeModal } from './QrCodeModal';
 
 export function ChildCard({ child }: { child: ChildViewDto }) {
   const [deleteSelectedChild, setSelectedChild, setOpenModal] =
---
+++ frontend-monitoramento-escolar/src/components/parent/children/QrCodeModal.tsx
@@ -0,0 +1,81 @@
+import {
+  CloseCircleFilled,
+  PrinterOutlined,
+  QrcodeOutlined,
+} from '@ant-design/icons';
+import { ChildViewDto } from '@backend/parent/dto/ChildViewDto';
+import { QRCodeDrawer } from '@frontend/components/common/QRcode/QRCodeDrawer';
+import { Button, Drawer, Flex, Typography } from 'antd';
+import dayjs from 'dayjs';
+
+export const QrCodeModal = ({
+  isOpen,
+  setIsOpen,
+  child,
+}: {
+  isOpen: boolean;
+  setIsOpen: (isOpen: boolean) => void;
+  child: ChildViewDto;
+}) => {
+  const onClose = () => {
+    setIsOpen(false);
+  };
+
+  const onPrint = () => {
+    window.print();
+  };
+  return (
+    <Drawer open={isOpen} onClose={onClose} width={'100vw'}>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'start',
+          gap: '1rem',
+          width: '100%',
+        }}
+      >
+        <Typography.Title level={4}>
+          QRCode de {child.name} {child.lastName} <QrcodeOutlined />
+        </Typography.Title>
+        <Flex gap='2px' vertical align='start'>
+          <Typography.Text>
+            Data de nascimento: {dayjs(child.birthDate).format('DD/MM/YYYY')}
+          </Typography.Text>
+          <Typography.Text>Ano escolar: {child.grade}</Typography.Text>
+        </Flex>
+        <Flex vertical align='center'>
+          <Typography.Text strong>
+            Escaneie o QRCode abaixo para liberar ou receber {child.name}{' '}
+            {child.lastName}
+          </Typography.Text>
+          <QRCodeDrawer
+            style={{
+              width: '750px',
+              height: '750px',
+            }}
+            qrCodePayload={String(child.id)}
+          />
+        </Flex>
+      </div>
+      <Flex gap='1rem'>
+        <Button
+          type='primary'
+          color='primary'
+          icon={<PrinterOutlined />}
+          onClick={onPrint}
+        >
+          Impimir ou compartilhar código
+        </Button>
+        <Button
+          type='primary'
+          color='danger'
+          icon={<CloseCircleFilled />}
+          onClick={onClose}
+        >
+          Fechar
+        </Button>
+      </Flex>
+    </Drawer>
+  );
+};
