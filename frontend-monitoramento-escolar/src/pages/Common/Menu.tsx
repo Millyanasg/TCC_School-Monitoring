620104a5ec2bbeb7db540138536007c5e223f796
Millyanasg <Millyanasg@gmail.com>
2024-10-18 12:42:08 -0300

    refactor: update import paths for menu components to reflect new directory structure

--- frontend-monitoramento-escolar/src/pages/Common/Menu.tsx
+++ frontend-monitoramento-escolar/src/pages/Common/Menu.tsx
@@ -2,10 +2,10 @@ import { useUserStore } from '@frontend/stores/user/user.store';
 import 'leaflet/dist/leaflet.css';
 
 import { Layout } from '@frontend/components/Layout/Layout';
-import { AdminMenu } from '@frontend/components/common/AdminMenu';
-import { DriverMenu } from '@frontend/components/common/DriverMenu';
-import { ParentMenu } from '@frontend/components/common/ParentMenu';
-import { SessionMenu } from '@frontend/components/common/SessionMenu';
+import { AdminMenu } from '@frontend/components/common/Menus/AdminMenu';
+import { DriverMenu } from '@frontend/components/common/Menus/DriverMenu';
+import { ParentMenu } from '@frontend/components/common/Menus/ParentMenu';
+import { SessionMenu } from '@frontend/components/common/Menus/SessionMenu';
 import { UserSetUpFrom } from '@frontend/components/common/UserSetUpFrom';
 export function Menu() {
   const userData = useUserStore((state) => state.userData);
