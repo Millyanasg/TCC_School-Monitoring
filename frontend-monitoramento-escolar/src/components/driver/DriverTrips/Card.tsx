cd94c70d541bdf751513caa7fd48404c74f23991
Millyana <Millyanamello@gmail.com>
2024-11-03 10:31:06 -0300

    feat: driver using geolocation for school address and child's address.

--- frontend-monitoramento-escolar/src/components/driver/DriverTrips/Card.tsx
+++ frontend-monitoramento-escolar/src/components/driver/DriverTrips/Card.tsx
@@ -1,22 +1,93 @@
-import { Typography } from 'antd';
-import { Card } from 'antd-mobile';
+import {
+  DirectionsRenderer,
+  GoogleMap,
+  LoadScript,
+} from '@react-google-maps/api';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 type TCard = {
   child: string;
-  andress: string;
+  addressChild: string;
   time: string;
-  duraction: string;
+  duration: string;
+  addressSchool: string;
 };
-export const CardDriverTrips = ({ andress, child, duraction, time }: TCard) => {
+
+export function CardDriverTrips({
+  addressChild,
+  child,
+  duration,
+  time,
+  addressSchool,
+}: TCard) {
+  const [directions, setDirections] =
+    useState<google.maps.DirectionsResult | null>(null);
+  const containerStyle = {
+    width: '100%',
+    height: '400px',
+  };
+
+  useEffect(() => {
+    const fetchDirections = async () => {
+      try {
+        const { data: childData } = await axios.get(
+          `https://maps.googleapis.com/maps/api/geocode/json`,
+          {
+            params: {
+              address: addressChild,
+              key: `${process.env.REACT_APP_GOOGLE_MAPS}`,
+            },
+          },
+        );
+
+        const childLocation = childData.results[0].geometry.location;
+
+        const { data: schoolData } = await axios.get(
+          `https://maps.googleapis.com/maps/api/geocode/json`,
+          {
+            params: {
+              address: addressSchool,
+              key: `${process.env.REACT_APP_GOOGLE_MAPS}`,
+            },
+          },
+        );
+
+        const schoolLocation = schoolData.results[0].geometry.location;
+        const directionsService = new google.maps.DirectionsService();
+        directionsService.route(
+          {
+            origin: childLocation,
+            destination: schoolLocation,
+            travelMode: google.maps.TravelMode.DRIVING,
+          },
+          (result, status) => {
+            if (status === google.maps.DirectionsStatus.OK) {
+              setDirections(result);
+            } else {
+              console.error(`error fetching directions ${result}`);
+            }
+          },
+        );
+      } catch (error) {
+        console.error('Geocoding error:', error);
+      }
+    };
+
+    fetchDirections();
+  }, [addressChild, addressSchool]);
   return (
-    <Card
-      title={child}
-      headerStyle={{ backgroundColor: '#f1f1f1' }}
-      style={{ border: '1px solid #c1c1c1', padding: '0px' }}
-    >
-      <Typography>{andress}</Typography>
-      <Typography>{duraction}</Typography>
-      <Typography>{time}</Typography>
-    </Card>
+    <div>
+      <div>Endereço inicial: {addressChild}</div>
+      <div>Endereço Final: {addressSchool}</div>
+      <div>Criança: {child}</div>
+      <div>Tempo estimado: {duration}</div>
+      <div>Hora: {time}</div>
+      <LoadScript googleMapsApiKey={`${process.env.REACT_APP_GOOGLE_MAPS}`}>
+        <GoogleMap mapContainerStyle={containerStyle} zoom={10}>
+          {directions && <DirectionsRenderer directions={directions} />}
+        </GoogleMap>
+      </LoadScript>
+    </div>
   );
-};
+}
