8c3904ba06dd17c62629776b5683cf46d61a393b
Millyanasg <Millyanasg@gmail.com>
2024-11-03 11:09:25 -0300

    feat: add token refresh after user data update in ParentFormSummary

--- frontend-monitoramento-escolar/src/components/parent/parentFrom/ParentFormSummary.tsx
+++ frontend-monitoramento-escolar/src/components/parent/parentFrom/ParentFormSummary.tsx
@@ -12,6 +12,7 @@ import { AddedChildCard } from './children/AddedChildCard';
 import { AddedHomeAddressCard } from './AddedHomeAddressCard';
 import { useUserStore } from '@frontend/stores/user/user.store';
 import { useNavigate } from 'react-router-dom';
+import { refreshToken } from '@frontend/services/common/auth.service';
 
 export function ParentFormSummary() {
   const navigate = useNavigate();
@@ -29,6 +30,7 @@ export function ParentFormSummary() {
         homeAddresses: homeAddressList,
       });
       await updateUserData();
+      await refreshToken();
       navigate('/');
       triggerNotification({
         content: 'Responsável cadastrado com sucesso',
