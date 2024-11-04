77e5fdb1538a71d78d2d3d594455aa8cb56bd1a7
Millyanasg <Millyanasg@gmail.com>
2024-11-04 16:14:08 -0300

    feat: add documentation comments for Footer, Header, and UseRouteProtector components

--- frontend-monitoramento-escolar/src/components/Layout/Footer.tsx
+++ frontend-monitoramento-escolar/src/components/Layout/Footer.tsx
@@ -1,12 +1,26 @@
 import { useUserStore } from '@frontend/stores/user/user.store';
 import { TabBar } from 'antd-mobile';
-import {
-  CheckShieldOutline,
-  EnvironmentOutline,
-  SetOutline,
-} from 'antd-mobile-icons';
+import { CheckShieldOutline, EnvironmentOutline } from 'antd-mobile-icons';
 import { useLocation, useNavigate } from 'react-router-dom';
 
+/**
+ * Footer component that displays a fixed navigation bar at the bottom of the screen.
+ * The navigation bar contains tabs that allow the user to navigate to different routes.
+ * The tabs displayed are based on the user's type (driver or parent).
+ *
+ * @component
+ * @returns {JSX.Element} The rendered Footer component.
+ *
+ * @example
+ * // Usage example
+ * <Footer />
+ *
+ * @remarks
+ * This component uses the `useUserStore` hook to access user data and the `useNavigate` and `useLocation` hooks from `react-router-dom` for navigation.
+ *
+ * @function
+ * @name Footer
+ */
 export const Footer = () => {
   const userData = useUserStore((state) => state.userData);
   const navigate = useNavigate();
@@ -23,10 +37,10 @@ export const Footer = () => {
       key: userData?.type === 'driver' ? '/driver/confirm' : '/parent/confirm',
       icon: <EnvironmentOutline fontSize={38} />,
     },
-    {
-      key: '/settings',
-      icon: <SetOutline fontSize={38} />,
-    },
+    // {
+    //   key: '/settings',
+    //   icon: <SetOutline fontSize={38} />,
+    // },
   ];
   return (
     <div
