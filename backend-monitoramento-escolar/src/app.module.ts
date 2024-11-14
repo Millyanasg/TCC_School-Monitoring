20724b2ce1e32aab06611e8b97e72b5f3761d8aa
Millyana <Millyanamello@gmail.com>
2024-11-14 13:22:07 -0300

    feat: add nest serve-static

--- backend-monitoramento-escolar/src/app.module.ts
+++ backend-monitoramento-escolar/src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ScheduleModule } from '@nestjs/schedule';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -8,17 +10,16 @@ import { AuthModule } from './auth/auth.module';
 import { ChildrenModule } from './children/children.module';
 import { validate } from './config/env-validade';
 import { CryptModule } from './crypt/crypt.module';
+import { DriverInviteModule } from './driver-invite/driver-invite.module';
 import { DriverModule } from './driver/driver.module';
 import { GpsGateway } from './gps/gps.gateway';
 import { GpsModule } from './gps/gps.module';
 import { HomeAddressModule } from './home-address/home-address.module';
+import { LocationModule } from './location/location.module';
 import { ParentModule } from './parent/parent.module';
 import { ParseTokenPipe } from './parseToken';
 import { PrismaModule } from './prisma/prisma.module';
-
-import { DriverInviteModule } from './driver-invite/driver-invite.module';
 import { UserModule } from './user/user.module';
-import { LocationModule } from './location/location.module';
 
 @Module({
   imports: [
@@ -27,6 +28,10 @@ import { LocationModule } from './location/location.module';
       isGlobal: true,
       envFilePath: ['.env'],
       validate: validate,
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, 'public'), // Path to the public folder
+      exclude: ['/api*'], // Exclude API routes from being served as static files
     }),
     GpsModule,
     AuthModule,
